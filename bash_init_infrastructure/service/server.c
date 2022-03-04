407fa2507ddfe31c5aac89fa9206cc704b785feb
etudiant <jeanchristophesanchez@laposte.net>
2022-03-04 12:25:45 +0100

    server en C sans le fork

---
+++ bash_init_infrastructure/service/server.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <strings.h>
+#include <sys/socket.h>
+#include <resolv.h>
+#include <arpa/inet.h>
+#include <errno.h>
+
+#define MAX_BUF		16
+
+int main(int argc, char *argv[]){   
+	if(argc!=2){
+		printf("Wrong call : ./server port\n");
+		exit(1);
+	}
+	int port = atoi(argv[1]);
+	int sockfd;
+	struct sockaddr_in self;
+	char buffer[MAX_BUF];
+
+	//create socket
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		perror("Socket creation error");
+		exit(errno);
+	}
+
+	//Initialize address/port structure
+	bzero(&self, sizeof(self));
+	self.sin_family = AF_INET;
+	self.sin_port = htons(port);
+	self.sin_addr.s_addr = INADDR_ANY;
+
+	// Assign a port number to the socket
+    if (bind(sockfd, (struct sockaddr*)&self, sizeof(self)) != 0)
+	{
+		perror("socket:bind()");
+		exit(errno);
+	}
+
+	// Make it a "listening socket". Limit to 30 connections
+	if (listen(sockfd, 30) != 0)
+	{
+		perror("socket:listen()");
+		exit(errno);
+	}
+
+	//Server run continuously
+	while (1)
+	{	int clientfd;
+		struct sockaddr_in client_addr;
+		int addrlen=sizeof(client_addr);
+
+		//accept an incomming connection
+		clientfd = accept(sockfd, (struct sockaddr*)&client_addr,(void *)&addrlen);
+		printf("%s:%d connected\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+		
+		//recv the id and print it
+		recv(clientfd, buffer, MAX_BUF, 0);
+		printf("id:%s\n", buffer);
+
+		//Echo back the received data to the client
+		//send(clientfd, buffer, recv(clientfd, buffer, MAX_BUF, 0), 0);
+
+		//Close data connection
+		close(clientfd);
+	}
+
+	//Clean up
+	close(sockfd);
+	return 0;
+}
